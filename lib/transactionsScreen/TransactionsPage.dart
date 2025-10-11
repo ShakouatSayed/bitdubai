83c05bd0ead626deabda46e3b82c0e26bf0d0d6a
Shakouat Hossen <sayedckb61@gmail.com>
2025-10-11 20:08:28 +0600

    bitcoin-app

--- lib/transactionsScreen/TransactionsPage.dart
+++ lib/transactionsScreen/TransactionsPage.dart
@@ -8,26 +8,51 @@ class TransactionsPage extends StatelessWidget{
   Widget build(BuildContext context){
     return Scaffold(
       backgroundColor: backgraoundColor,
-      // appBar: AppBar(
-      //   backgroundColor: Color(0xFF0A8ED9),
-      //   //toolbarHeight: 100,
-      //   shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      //   leading: IconButton(onPressed: (){
-      //     Navigator.push(context, MaterialPageRoute(builder: (context) => StartPage()));
-      //   }, icon: Icon(Icons.arrow_back)),
-      //   title: Text("Transactions", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-      // ),
-      body: Container(
-        //color: Color(0xFF0A8ED9),
-        width: double.infinity,
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(12), color: Color(0xFF0A8ED9)),
-        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-
+               _buildCustomAppbar(),
           ],
         ),
+      )
+    );
+  }
+  Widget _buildCustomAppbar(){
+    return  Container(
+      //color: Color(0xFF0A8ED9),
+      width: double.infinity,
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(26), color: Color(0xFF0A8ED9)),
+      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 30),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              IconButton(onPressed: (){
+                //Navigator.push(context, MaterialPageRoute(builder: (context) => StartPage()));
+              }, icon: Icon(Icons.arrow_back)),
+              SizedBox(width: 10),
+              Text("Transactions", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            ],
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "Search Transactions...",
+                prefixIcon: Icon(Icons.search,size: 30),
+                filled: true,
+                fillColor: Color(0xFF10B1D3),
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(12), borderSide: BorderSide.none),
+              ),
+            ),
+          ),
+          SizedBox(height: 10)
+        ],
       ),
     );
   }
