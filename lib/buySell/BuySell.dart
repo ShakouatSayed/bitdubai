1a2246a4077bdc3c702ef8716abfcb3344e1fc52
user <sayedckb61@gmail.com>
2025-10-14 18:16:12 +0600

    14/10/2025

--- lib/buySell/BuySell.dart
+++ lib/buySell/BuySell.dart
@@ -13,13 +13,25 @@ class BuySell extends StatelessWidget{
       body: Obx((){
         if(controller.assets.isEmpty) {
           return Center(child: CircularProgressIndicator());
-        } return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            _buildCustomAppbar(),
-            SizedBox(height: 20),
-            _buildAssetSelection(),
-          ],
+        } return SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildCustomAppbar(),
+              SizedBox(height: 20),
+              _buildAssetSelection(),
+              SizedBox(height: 10),
+              _buildPaymentSections(),
+              SizedBox(height: 10),
+              _buildExchangeSection(),
+              SizedBox(height: 10),
+              _buildQuickAmounts(),
+              SizedBox(height: 10),
+              _buildConfirmButton(),
+              SizedBox(height: 10),
+              _buildInfoCard(),
+            ],
+          ),
         );
       }),
       
@@ -79,6 +91,7 @@ class BuySell extends StatelessWidget{
       ),
     );
   }
+  
   Widget _buildAssetSelection() {
     return Obx(() => Row(
       mainAxisAlignment:  MainAxisAlignment.spaceEvenly,
@@ -129,6 +142,7 @@ class BuySell extends StatelessWidget{
     return Obx((){
       final selected = controller.selectedAsset.value!;
       return Container(
+        margin: EdgeInsets.symmetric(horizontal: 10),
         padding: EdgeInsets.all(16),
         decoration: BoxDecoration(
           color: Colors.white,
@@ -136,7 +150,7 @@ class BuySell extends StatelessWidget{
           boxShadow:[
             BoxShadow(
               color: Colors.grey.shade300,
-              blurRadius: 6,
+              blurRadius: 15,
               offset: Offset(0, 2),
             ),
           ]
@@ -147,14 +161,126 @@ class BuySell extends StatelessWidget{
             _buildRow("You Pay", controller.currency.value),
             TextField(
               keyboardType: TextInputType.number,
-              decoration: InputDecoration(hintText: "0.00"),
+              //textInputAction: TextInputAction.none,
+              decoration: InputDecoration(hintText: "0.00", border: InputBorder.none),
+              //enabled: false,
               onChanged: (val) => controller.updatePay(double.tryParse(val) ?? 0),
             ),
-            SizedBox(height: 10),
+
+            Text("Balance: ${controller.balance.value} AED", style: TextStyle(fontSize: 16, color: Colors.blueAccent)),
+            SizedBox(height: 40),
+            Center(
+              child: Icon(Icons.swap_vert, color: Colors.blue, size: 30),
+            ),
+            _buildRow("You Receive", selected.symbol),
+            Text(controller.receiveAmount.value.toStringAsFixed(6), style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
           ],
         ),
       );
     });
+  }
+
+  Widget _buildExchangeSection()  {
+    return Obx(() {
+      final selected = controller.selectedAsset.value!;
+      final rate = controller.mode.value == "Buy" ? selected.buyRate : selected.sellRate;
+
+      return Container(
+        margin: EdgeInsets.symmetric(horizontal: 10),
+        padding: EdgeInsets.all(16),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12),
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 15,
+              color: Colors.grey,
+              offset: Offset(0, 2),
+            ),
+          ]
+        ),
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+           Row(
+             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+             children: [
+               Text("Exchange Rate: "),
+               Text("1${selected.symbol} = $rate ${controller.currency.value}"),
+             ],
+           ),
+           Row(
+             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+             children: [
+               Text("Fee: "),
+               Text("${controller.fee.toStringAsFixed(2)} ${controller.currency.value} (${controller.feePercent.value}%)"),
+             ],
+           ),
+            Divider(thickness: 1, color: Colors.grey),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Total: "),
+                Text("${controller.total.toStringAsFixed(2)} ${controller.currency.value}"),
+              ],
+            ),
+
+
+          ],
+        ),
+      );
+    });
+  }
+
+  Widget _buildQuickAmounts(){
+    return Obx(() => Padding(
+      padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+      child: Wrap(
+        spacing: 28,
+        runSpacing: 10,
+        // crossAxisAlignment: WrapCrossAlignment.end,
+        // alignment: WrapAlignment.spaceAround,
+        children: controller.amountOptions.map((amt){
+          return OutlinedButton(onPressed:() => controller.updatePay(amt.toDouble()), child: Text("$amt"));
+        }).toList(),
+      ),
+    ));
+  }
+
+  Widget _buildConfirmButton() {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+      child: SizedBox(
+        width: double.infinity,
+        height: 50,
+        child: ElevatedButton(onPressed: (){},style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.blue,
+          padding: EdgeInsets.symmetric(horizontal: 60, vertical: 14),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        ), child: Obx(() => Text(controller.mode.value =="Buy" ? "Confirm Buy" : "Confirm Sell", style: TextStyle(color: Colors.white))),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildInfoCard(){
+    return Obx(() => Container(
+      padding: EdgeInsets.all(16),
+      margin: EdgeInsets.symmetric(horizontal: 10),
+      decoration: BoxDecoration(
+        color: Colors.amber.shade50,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        children: [
+          Icon(Icons.info_outline, color: Colors.amber),
+          SizedBox(width: 10),
+          Expanded(child: Text(controller.infoText.value)),
+        ],
+      ),
+    ));
   }
 
   Widget _buildRow(String left, String right){
